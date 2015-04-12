78fb61c050105bdf8ac1f50ea820e9751961b2f3
mapolovina <marko.polovina.geof@gmail.com>
2015-04-12 14:25:03 +0200

    sf

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,7 +4,7 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
     $ionicPlatform.ready(function() {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
-      if(window.cordova && window.cordova.plugins.Keyboard) {
+      if(window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       }
       if(window.StatusBar) {
@@ -28,7 +28,7 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
     }
 })
 
-.controller('ReportCtrl', function($scope, $ionicPopup, $cordovaCamera, Camera) {
+.controller('ReportCtrl', function($scope, $ionicPopup, $cordovaCamera) {
 
     var destinationType = $cordovaCamera.DestinationType,
     pictureSource = $cordovaCamera.PictureSourceType;
