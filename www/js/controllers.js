5de25cdbe91d2d6cfdfb9ad8ca584e047b855656
mapolovina <marko.polovina.geof@gmail.com>
2015-04-12 14:08:42 +0200

    sf

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,18 @@
 angular.module('starter.controllers', ['ionic', 'ngCordova'])
 
+.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+})
+
 .controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
     $scope.data = {};
 
